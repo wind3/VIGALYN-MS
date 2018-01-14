85a922fe2a4815fb427367b080a51efa729cee4c
曾维嘉 <49917747@qq.com>
2018-01-14 14:40:44 +0800

    complete config server and config client

--- vigalyn-ms-www/src/main/java/com/vigalyn/ms/www/VigalynMsWwwApplication.java
+++ vigalyn-ms-www/src/main/java/com/vigalyn/ms/www/VigalynMsWwwApplication.java
@@ -2,11 +2,11 @@ package com.vigalyn.ms.www;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableFeignClients
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class VigalynMsWwwApplication {
 
--- vigalyn-ms-www/src/main/java/com/vigalyn/ms/www/controller/HelloController.java
+++ vigalyn-ms-www/src/main/java/com/vigalyn/ms/www/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.vigalyn.ms.www.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,15 +16,19 @@ import com.vigalyn.ms.www.service.WwwHelloService;
 
 @RestController
 @RequestMapping(value="/hello",produces= {"application/json;charset=UTF-8"})
+@RefreshScope
 public class HelloController {
     private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
     @Autowired
     private WwwHelloService wwwHelloService;
     
+    @Value("${wind3:age}")
+    String school = "桂平三中";
+    
     @RequestMapping(value="/name",method=RequestMethod.GET)
     public String hello(@RequestParam String name) {
-        LOG.info("begin hello " + name);
-        return wwwHelloService.hello(name);
+        LOG.info("begin hello " + name + ";school =" + school);
+        return wwwHelloService.hello(name + school);
     }
     
     @RequestMapping(value="/user",method=RequestMethod.GET)
