7ec2432273ba2407e0b51d87904b74ee69226038
曾维嘉 <49917747@qq.com>
2018-01-23 22:35:50 +0800

    modify config

--- vigalyn-ms-www/src/main/java/com/vigalyn/ms/www/config/HystrixConfig.java
+++ vigalyn-ms-www/src/main/java/com/vigalyn/ms/www/config/HystrixConfig.java
@@ -3,8 +3,6 @@ package com.vigalyn.ms.www.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.vigalyn.ms.www.service.impl.WwwHelloServiceHystrixImpl;
-
 import feign.Feign;
 
 @Configuration
